d3c5f9b4902a66c2f01db964aefe3045a6938c49
Caitlin Conway <coolcac@gmail.com>
2021-01-22 22:53:25 -0600

    pic display working

--- private-skeleton/client/src/components/FaceTile.js
+++ private-skeleton/client/src/components/FaceTile.js
@@ -5,12 +5,11 @@ const FaceTile = ({ person})=> {
   // function handleClick() {
   //   return (<Redirect to={`/persons/${person}`}></Redirect>)
   // }
+  let pic = require(`../images/${person}.PNG`);
   return (
     <>
       <div id={'face-tile'}>
-        <div className={'face-pic'}>
-          {person.pic}
-        </div>
+        <img className={'face-pic'} src={pic} width = '100' height='100px'/>
         <div className={'face-name'}>
           {person}
         </div>
