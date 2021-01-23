1a4d013c95add01957b5e37b294d8d8dc73be656
Caitlin Conway <coolcac@gmail.com>
2021-01-22 23:04:29 -0600

    styling started

--- private-skeleton/client/src/components/FaceTile.js
+++ private-skeleton/client/src/components/FaceTile.js
@@ -10,9 +10,9 @@ const FaceTile = ({ person})=> {
     <>
       <div id={'face-tile'}>
         <img className={'face-pic'} src={pic} width = '100' height='100px'/>
-        <div className={'face-name'}>
+        {/* <div className={'face-name'}>
           {person}
-        </div>
+        </div> */}
       </div>
     </>
   )
