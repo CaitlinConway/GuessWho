d0f159fcbf58eafaac2543a143bfead3471c2a38
Caitlin Conway <coolcac@gmail.com>
2021-01-26 12:57:05 -0600

    click handler added

--- private-skeleton/client/src/components/FaceTile.js
+++ private-skeleton/client/src/components/FaceTile.js
@@ -2,21 +2,31 @@ import React from "react";
 import { Redirect, NavLink, Route } from 'react-router-dom';
 
 const FaceTile = ({ person})=> {
-  function handleClick() {
-
+  const [showX, setShowX] = React.useState(false);
+  const handleClick = (e) => {
+    e.preventDefault();
+    setShowX(true);
   }
   let pic = require(`../images/${person}.PNG`);
+  let x_pic = require('../images/X.png');
+  const X_picture = (
+    <div id={`x-${person}`}>
+    <img className={'x'} src ={x_pic} width ='100' height = '100'/>
+    </div>
+    )
   return (
     <>
-      <div id={'face-tile'}>
-        <img className={'face-pic'} src={pic} width = '100' height='100px' onClick={handleClick}/>
+      <div id={'face-tile'} onClick={handleClick}>
+        <img className={'face-pic'} src={pic} width = '100' height='100px'/>
         {/* <div className={'face-name'}>
           {person}
         </div> */}
+        {showX ? X_picture:null}
       </div>
     </>
   )
 };
+
 
 
 export default FaceTile;
