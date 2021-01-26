c8027ba9b54b8ef2b264e71a530949fc744c6e1d
Caitlin Conway <coolcac@gmail.com>
2021-01-26 13:10:34 -0600

    positioning added

--- private-skeleton/client/src/components/FaceTile.js
+++ private-skeleton/client/src/components/FaceTile.js
@@ -10,17 +10,12 @@ const FaceTile = ({ person})=> {
   let pic = require(`../images/${person}.PNG`);
   let x_pic = require('../images/X.png');
   const X_picture = (
-    <div id={`x-${person}`}>
-    <img className={'x'} src ={x_pic} width ='100' height = '100'/>
-    </div>
+    <img className={'x'} src ={x_pic} width ='70' height = '70'/>
     )
   return (
     <>
-      <div id={'face-tile'} onClick={handleClick}>
+      <div className={'face-tile'} onClick={handleClick}>
         <img className={'face-pic'} src={pic} width = '100' height='100px'/>
-        {/* <div className={'face-name'}>
-          {person}
-        </div> */}
         {showX ? X_picture:null}
       </div>
     </>
