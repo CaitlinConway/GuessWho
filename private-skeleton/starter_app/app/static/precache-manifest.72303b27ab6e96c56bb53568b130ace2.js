821e51a898ce74a0a7fb6d9275b45a7281abf913
Caitlin Conway <coolcac@gmail.com>
2021-01-26 15:15:54 -0600

    dockerfile updated

---
+++ private-skeleton/starter_app/app/static/precache-manifest.72303b27ab6e96c56bb53568b130ace2.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07cfa14fe87b689eccd1cda0b1b1349b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03ca195d224d37d29a34",
+    "url": "/static/css/main.491b1362.chunk.css"
+  },
+  {
+    "revision": "30933990529bc27d1bd6",
+    "url": "/static/js/2.cf5da9c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cf5da9c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ca195d224d37d29a34",
+    "url": "/static/js/main.5dc626da.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "edcd52399a53683da0bacf5b1abf03dd",
+    "url": "/static/media/Alex.edcd5239.PNG"
+  },
+  {
+    "revision": "e403931e752d927206497ff3d50585ac",
+    "url": "/static/media/Alfred.e403931e.PNG"
+  },
+  {
+    "revision": "be008815f7b68167b56e3b7cc741bd7a",
+    "url": "/static/media/Anita.be008815.PNG"
+  },
+  {
+    "revision": "e59607df48cc145b98f41f094071b29b",
+    "url": "/static/media/Anne.e59607df.PNG"
+  },
+  {
+    "revision": "ab4b2537e84750528d4f9b5a0c35a382",
+    "url": "/static/media/Bernard.ab4b2537.PNG"
+  },
+  {
+    "revision": "b13d8fe3a7887f81e6c8bb6d006681ee",
+    "url": "/static/media/Bill.b13d8fe3.PNG"
+  },
+  {
+    "revision": "89c8ec3a5310a536833290247c2e2e29",
+    "url": "/static/media/Charles.89c8ec3a.PNG"
+  },
+  {
+    "revision": "70b2ab64385d4dbdf050eae8fcda22af",
+    "url": "/static/media/Claire.70b2ab64.PNG"
+  },
+  {
+    "revision": "94c6d5114a5aba3be767928687401670",
+    "url": "/static/media/David.94c6d511.PNG"
+  },
+  {
+    "revision": "04f68d1185bb25c980db9950302028e2",
+    "url": "/static/media/Eric.04f68d11.PNG"
+  },
+  {
+    "revision": "81f315948aca254ecd978043a6d00e6f",
+    "url": "/static/media/Frans.81f31594.PNG"
+  },
+  {
+    "revision": "3b774c3da1c922d56c6131528cfaaef6",
+    "url": "/static/media/George.3b774c3d.PNG"
+  },
+  {
+    "revision": "0812537040f85ee4fc6c7f50583d21ec",
+    "url": "/static/media/Herman.08125370.PNG"
+  },
+  {
+    "revision": "93c0afff18be713dfc01a0cef2792f24",
+    "url": "/static/media/Joe.93c0afff.PNG"
+  },
+  {
+    "revision": "0465a1cddc57e3cc51b33ee6f59f25d0",
+    "url": "/static/media/Maria.0465a1cd.PNG"
+  },
+  {
+    "revision": "4ba37fd899ae0fbc971799b3c5fd7935",
+    "url": "/static/media/Max.4ba37fd8.PNG"
+  },
+  {
+    "revision": "883a40c074ded82a04998d823f5262d1",
+    "url": "/static/media/Paul.883a40c0.PNG"
+  },
+  {
+    "revision": "8f7dc28507a0ab5c9966779858f35d55",
+    "url": "/static/media/Peter.8f7dc285.PNG"
+  },
+  {
+    "revision": "df9f1b880c516ff1d4e5d39cbdbc07ca",
+    "url": "/static/media/Philip.df9f1b88.PNG"
+  },
+  {
+    "revision": "55c2d50c2da794d5daf5e0ef181f209d",
+    "url": "/static/media/Richard.55c2d50c.PNG"
+  },
+  {
+    "revision": "3c9c02659c159f18ba16507a2d23e1b5",
+    "url": "/static/media/Robert.3c9c0265.PNG"
+  },
+  {
+    "revision": "c71134b1d46dfd686de2aa5b2bd83679",
+    "url": "/static/media/Sam.c71134b1.PNG"
+  },
+  {
+    "revision": "6a36f5e4b361df042b557f96d8b88b2a",
+    "url": "/static/media/Susan.6a36f5e4.PNG"
+  },
+  {
+    "revision": "340a892f68e5ea9d0843ea41691accbd",
+    "url": "/static/media/Tom.340a892f.PNG"
+  },
+  {
+    "revision": "17dfc335a9f8e277dc5d22f9abefe3c5",
+    "url": "/static/media/X.17dfc335.png"
+  }
+]);
